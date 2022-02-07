a1e7670a64d1f911db720bd8d08a8c0155d9070f
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-02-07 17:33:21 -0500

    commit tree e68884e94f6d5bc7a86a7d6ef151a7ba905bec4e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function superbowlWin(record) {
+  let winner = record.find((ass) => ass.result === "W");
+  if (winner) return winner.year;
+  // else return winner.year;
+}
